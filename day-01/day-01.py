8d772fa2be698c570154defb038ae1af2c2b4759
Patrick Pichler <ppichler@outlook.at>
2021-12-07 10:45:06 +0100

    solves second part of day 1

--- day-01/day-01.py
+++ day-01/day-01.py
@@ -2,34 +2,49 @@ import unittest
 
 class TestDay01(unittest.TestCase):
 
-    def test_example(self):
-        input = """ 199
-                    200
-                    208
-                    210
-                    200
-                    207
-                    240
-                    269
-                    260
-                    263 """
+    def test_example_A(self):
+        inputFile = open("day-01/example.txt", "r")
+        input = inputFile.readlines()
+        inputFile.close()
 
-        result = day01(input.split("\n"))
+        result = day01A(input)
 
-        print (f'Example result: {result}')
+        print (f'Example result A: {result}')
         self.assertEqual(result, 7)
 
 
-    def test_puzzleInput(self):
+    def test_puzzleInput_A(self):
         inputFile = open("day-01/input.txt", "r")
         input = inputFile.readlines()
+        inputFile.close()
 
-        result = day01(input)
+        result = day01A(input)
 
-        print (f'puzzle data result: {result}')
+        print (f'puzzle data result A: {result}')
 
 
-def day01(input):
+    def test_example_B(self):
+        inputFile = open("day-01/example.txt", "r")
+        input = inputFile.readlines()
+        inputFile.close()
+
+        result = day01B(input)
+
+        print (f'Example result B: {result}')
+        self.assertEqual(result, 5)
+
+
+    def test_puzzleInput_B(self):
+        inputFile = open("day-01/input.txt", "r")
+        input = inputFile.readlines()
+        inputFile.close()
+
+        result = day01B(input)
+
+        print (f'puzzle data result B: {result}')
+
+
+def day01A(input):
     increase = 0
     previous = int(input.pop(0))
     for current in input:
@@ -39,6 +54,25 @@ def day01(input):
         previous = value
 
     return increase
+
+
+def day01B(input):
+    depthOfWindow = []
+    for i in range(0, len(input)-2):
+        sum = 0
+        for j in range(0, 3):
+            sum += int(input[i+j])
+        depthOfWindow.append(sum)
+
+    increase = 0
+    previous = depthOfWindow.pop(0)
+    for value in depthOfWindow:
+        if (value > previous):
+            increase += 1
+        previous = value
+
+    return increase
+
 
 if __name__ == "__main__":
     unittest.main()
