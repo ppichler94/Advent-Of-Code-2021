ab1fe2e6233780b8eff224a198381dc241266026
Patrick Pichler <ppichler@outlook.at>
2022-01-09 13:11:19 +0100

    solves part 2 of day 21

--- day-21/day-21.py
+++ day-21/day-21.py
@@ -1,5 +1,6 @@
 import os
 from typing import Tuple
+from functools import lru_cache
 
 
 class DeterministicDice:
@@ -24,13 +25,38 @@ def read_input_from_file(file_name: str) -> list:
     return data
 
 
-def move_player(position: int, points: int, dice: DeterministicDice) -> Tuple[int, int]:
-    for _ in range(3):
-        position += dice.roll()
+def update_position(position: int, count: int) -> int:
+    position += count
     while position > 10:
         position -= 10
+    return position
+
+
+def move_player(position: int, points: int, dice: DeterministicDice) -> Tuple[int, int]:
+    movement = sum(dice.roll() for _ in range(3))
+    position = update_position(position, movement)
     points += position
     return (position, points)
+
+
+@lru_cache(maxsize=None)
+def do_quantum_turn(pos1: int, pos2: int, points1: int, points2: int) -> Tuple[int, int]:
+    possibilities = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+    wins1, wins2 = 0, 0
+
+    for movement, splits in possibilities.items():
+        pos = update_position(pos1, movement)
+        points = points1 + pos
+        if points >= 21:
+            wins1 += splits
+            continue
+        pos = update_position(pos1, movement)
+        next_wins2, next_wins1 = do_quantum_turn(pos2, pos, points2, points)
+        wins1 += next_wins1 * splits
+        wins2 += next_wins2 * splits
+
+    return (wins1, wins2)
 
 
 def get_starting_positions(data: list) -> Tuple[int, int]:
@@ -52,11 +78,19 @@ def part_1(data):
             return points_a * dice.get_roll_count()
 
 
+def part_2(data):
+    pos1, pos2 = get_starting_positions(data)
+    wins = do_quantum_turn(pos1, pos2, 0, 0)
+    print(f"Part 2: {wins}")
+
+
 def main() -> None:
     data = read_input_from_file("input.txt")
 
     result_1 = part_1(data)
     print(f"Part 1: {result_1}")
+
+    part_2(data)
 
 
 if __name__ == "__main__":
